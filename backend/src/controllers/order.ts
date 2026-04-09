b7e98465f68286b0a6eaafb7731c5f2aeee4cd61
malannly <mamedovaanna.i@gmail.com>
2026-04-09 16:09:44 +0300

    сдача проектной работы

--- backend/src/controllers/order.ts
+++ backend/src/controllers/order.ts
@@ -13,32 +13,32 @@ interface Order {
 }
 
 const setOrder = async (req: Request, res: Response, next: NextFunction) => {
+  const {
+    payment, email, phone, address, total, items,
+  }: Order = req.body;
+
+  // check if all filds are filled
+  if (!payment || !email || !address || !phone || total === undefined || !items) {
+    return next(new BadRequestError('all fields are required'));
+  }
+
+  // checks if the payment is by card or cash
+  if (!['card', 'online'].includes(payment)) {
+    return next(new BadRequestError('the payment has to be chosen either cash or card'));
+  }
+
+  // static method of checking the email
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!regex.test(email)) {
+    return next(new BadRequestError('wrong email'));
+  }
+
+  // checks if the items is an array
+  if (!Array.isArray(items) || items.length === 0) {
+    return next(new BadRequestError('items cannot be an empty string'));
+  }
+
   try {
-    const {
-      payment, email, phone, address, total, items,
-    }: Order = req.body;
-
-    // check if all filds are filled
-    if (!payment || !email || !address || !phone || total === undefined || !items) {
-      return next(new BadRequestError('all fields are required'));
-    }
-
-    // checks if the payment is by card or cash
-    if (!['card', 'online'].includes(payment)) {
-      return next(new BadRequestError('the payment has to be chosen either cash or card'));
-    }
-
-    // static method of checking the email
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if (!regex.test(email)) {
-      return next(new BadRequestError('wrong email'));
-    }
-
-    // checks if the items is an array
-    if (!Array.isArray(items) || items.length === 0) {
-      return next(new BadRequestError('items cannot be an empty string'));
-    }
-
     const products = await Product.find({ _id: { $in: items } });
 
     if (products.length !== items.length) {
@@ -62,8 +62,7 @@ const setOrder = async (req: Request, res: Response, next: NextFunction) => {
       total,
     });
   } catch (error) {
-    console.error('Error In setOrder:', error);
-    return next(new BadRequestError('Error due to creation of the order'));
+    return next(new BadRequestError('Error during the creation of the order'));
   }
 };
 
